0b4486a020f3873c55d7fb9dbfcc7b35bf5ed6fb
Derek O'Kelly <derekokelly1997@gmail.com>
2018-10-24 14:57:01 +0100

    remove some print statements

--- ErlangB.py
+++ ErlangB.py
@@ -8,9 +8,9 @@ import matplotlib.patches as patches
 from phoneLine import PhoneLine
 
 # Amount of phone lines
-n = 30
+n = 25
 # List of traffic for Erlang calculation
-predicted_amount_of_calls = [500]
+amount_of_calls = [500]
 
 # Main function
 def main():
@@ -18,7 +18,7 @@ def main():
     print("\n--------\n")
 
     # Print amount of calls & lines
-    for call in predicted_amount_of_calls:
+    for call in amount_of_calls:
         print("Traffic: ", call, " calls")
         print("Number of lines: ", n)
         print("\n--------\n")
@@ -26,7 +26,7 @@ def main():
     # Generate call start times based off a random uniform distribution
     call_start_times = sorted(numpy.random.uniform(0, 60, call))
     # Generate call holding times based off gamma distribution
-    call_length = numpy.random.standard_gamma(10, call)
+    call_length = numpy.random.standard_gamma(3, call)
     
     # Get average call length
     ave_call_length = (sum(call_length) / len(call_length))
@@ -43,16 +43,9 @@ def main():
 
     # Loop through calls
     for i in range(0, call):
-        print("\n--------\n")
-        print("Starting simulation on call ", i, "\n")
         call_accepted = False
         current_call_start = call_start_times[i]
         current_call_end = (call_start_times[i] + call_length[i])
-        current_call_duration = call_length[i]
-
-        print("Call start time:", current_call_start)
-        print("Call end time:", current_call_end)
-        print("Call duration:", current_call_duration)
 
         line_number = 0
 
@@ -60,23 +53,12 @@ def main():
         for line in phone_lines:
             line_number += 1
 
-            print("\nChecking line: ", line_number)
-            print("Line call start time: ", line.call_start)
-            print("Line call end time: ", line.call_end)
-
             # Check if line is free
             if line.call_end <= current_call_start:
                 line.call_start = current_call_start
                 line.call_end = current_call_end
                 call_accepted = True
-                print("\nCall accepted on line ", line_number)
                 break
-            else:
-                # Check if we have lines left
-                if line_number == n:
-                    print("\nNo lines were free, call rejected.")
-                else:
-                    print("\nLine ", line_number, " is busy. Trying next line.")
 
         # Tally calls accepted & rejected
         if call_accepted:
@@ -93,7 +75,7 @@ def main():
     traffic = call * (ave_call_length / 60)
     erlang_gos = ErlangB(n, traffic)
     print("Erlang GOS: ", erlang_gos)
-    print("Predicted GOS: ", (num_calls_rejected/call))
+    print("Simulated GOS: ", (num_calls_rejected/call))
 
     print("\n--------\n")
 
